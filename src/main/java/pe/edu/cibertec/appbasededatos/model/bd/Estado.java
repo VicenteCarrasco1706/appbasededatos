2cd66af309b76559c27b2914f285d30e74047027
VicenteCarrasco1706 <carrascovicente88@gmail.com>
2023-05-07 10:25:19 -0500

    mensaje

--- src/main/java/pe/edu/cibertec/appbasededatos/model/bd/Estado.java
+++ src/main/java/pe/edu/cibertec/appbasededatos/model/bd/Estado.java
@@ -2,6 +2,8 @@ package pe.edu.cibertec.appbasededatos.model.bd;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,6 +16,7 @@ import lombok.Setter;
 @Table(name = "estado")
 public class Estado {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idestado;
 	@Column(name= "descestado")
 	private String descestado;
